d639a87d18dadfe78caad566fbd1e5bbde85cc33
KMijat <kristina.mijatovic04@htl-salzburg.ac.at>
2024-02-01 01:08:28 +0100

    final upload

---
+++ Assets/MyGame/Script/Question.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+
+    public string frageText;
+    public string antwort1;
+    public string antwort2;
+    public string antwort3;
+    //public string id;
+
+    public int richtigeAntwort;
+
+}
